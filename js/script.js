6b8c4cf89814a3622df1b298f53e7932caea411b
plaidtrack <tartanrails572@gmail.com>
2025-12-08 22:19:23 -0600

    Rotating text script for header text

--- js/script.js
+++ js/script.js
@@ -82,3 +82,16 @@ if (videoModal !== null) {
     video.setAttribute('src', videoSrc);
   });
 }
+
+// Replace Text In Header
+const checkReplace = document.querySelector('.replace-me');
+
+if (checkReplace !== null) {
+  const replace = new ReplaceMe(checkReplace, {
+    animation: 'animated fadeIn',
+    speed: 2000,
+    separator: ',',
+    loopCount: 'infinite',
+    autoRun: true,
+  });
+}
